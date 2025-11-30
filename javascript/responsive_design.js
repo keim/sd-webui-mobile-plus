2c8f496a937d7e474d03e9f1fd4284d4bc4ef406
keim <mesudak@gmail.com>
2025-12-01 05:33:04 +0900

    helper buttons

--- javascript/responsive_design.js
+++ javascript/responsive_design.js
@@ -18,6 +18,22 @@ function insertPanel() {
     _setOnClick('sspp-size', e => {
         panel.classList.toggle("size-select");
     });
+    _setOnClick('sspp-prevword', e => {
+        sspp_selectPrevWord(panel.classList.contains("word-select"))
+    });
+    _setOnClick('sspp-currword', e => {
+        sspp_selectWord(panel.classList.contains("word-select"));
+        panel.classList.toggle("word-select");
+    });
+    _setOnClick('sspp-nextword', e => {
+        sspp_selectNextWord(panel.classList.contains("word-select"))
+    });
+    _setOnClick('sspp-ratedown', e => {
+        _sspp_insideBlacket(_promptTextArea(), -0.05);
+    });
+    _setOnClick('sspp-rateup', e => {
+        _sspp_insideBlacket(_promptTextArea(), +0.05);
+    });
 
     sspp_updateSizeDisplay();
     root.appendChild(panel)
@@ -27,12 +43,30 @@ function insertPanel() {
 }
 
 
+// 現在選択されているタブの名前を取得する
+function _currentTab() {
+    return document.querySelector('#tabs button.selected').textContent.trim().toLowerCase();
+}
+
+// 現在のタブに対応するプロンプトテキストエリアを取得する
+function _promptTextArea() {
+    const tab = _currentTab();
+    if (tab === 'txt2img') {
+        return document.querySelector('#txt2img_prompt textarea');
+    } else if (tab === 'img2img') {
+        return document.querySelector('#img2img_prompt textarea');
+    }
+    return null;
+}
+
+
 // パネルにボタンを追加するユーティリティ関数
 function _setOnClick(id, onClick) {
     document.getElementById(id).addEventListener('click', onClick);
 }
 
 
+// サイズ選択ボタンが押されたときの処理
 function sspp_setSize(width, height) {
     const widthInput = document.querySelector('#txt2img_width input');
     const heightInput = document.querySelector('#txt2img_height input');
@@ -44,12 +78,91 @@ function sspp_setSize(width, height) {
     sspp_updateSizeDisplay();
 }
 
+// サイズ表示を更新する
 function sspp_updateSizeDisplay() {
     const widthInput = document.querySelector('#txt2img_width input');
     const heightInput = document.querySelector('#txt2img_height input');
     const sizeLabel = document.querySelector('#sspp-size>span.sspp-button-label');
     sizeLabel.textContent = `${widthInput.value}x${heightInput.value}`;
 }
+
+
+// テキストエリア内の現在の単語を選択/解除する
+function sspp_selectWord(hold) {
+    const textArea = _promptTextArea();
+    if (!textArea) return;
+    if (hold) _sspp_unselectWord(textArea);
+    else _sspp_selectCurrentWord(textArea);
+}
+function _sspp_selectCurrentWord(textArea) {
+    const text = textArea.value;
+    let start = textArea.selectionStart;
+    while (start > 0 && !/\s/.test(text[start - 1])) start--;
+    let end = textArea.selectionEnd;
+    while (end < text.length && !/\s/.test(text[end])) end++;
+    textArea.setSelectionRange(start, end);
+    textArea.focus();
+}
+function _sspp_unselectWord(textArea) {
+    const pos = textArea.selectionEnd;
+    textArea.setSelectionRange(pos, pos);
+    textArea.focus();
+}
+
+// テキストエリア内の前の単語を選択する
+function sspp_selectPrevWord(hold) {
+    const textArea = _promptTextArea();
+    if (!textArea) return;
+    _sspp_selectPrevWord(textArea, hold);
+}
+function _sspp_selectPrevWord(textArea, hold) {
+    _sspp_selectCurrentWord(textArea);
+    const text = textArea.value;
+    let end = textArea.selectionStart;
+    if (end === 0) return;
+    end--;
+    while (end > 0 && /\s/.test(text[end - 1])) end--;
+    let start = end;
+    while (start > 0 && !/\s/.test(text[start - 1])) start--;
+    textArea.setSelectionRange(start, hold ? textArea.selectionEnd : end);
+    textArea.focus();
+}
+
+// テキストエリア内の次の単語を選択する
+function sspp_selectNextWord() {
+    const textArea = _promptTextArea();
+    if (!textArea) return;
+    _sspp_selectNextWord(textArea, hold);
+}
+function _sspp_selectNextWord(textArea, hold) {
+    _sspp_selectCurrentWord(textArea);
+    const text = textArea.value;
+    let start = textArea.selectionEnd;
+    if (start === text.length) return;
+    while (start < text.length && /\s/.test(text[start])) start++;
+    let end = start;
+    while (end < text.length && !/\s/.test(text[end])) end++;
+    textArea.setSelectionRange(hold ? textArea.selectionStart : start, end);
+    textArea.focus();
+}
+
+function _sspp_insideBlacket(textArea, rateGain) {
+    const text = textArea.value;
+    let start = textArea.selectionStart;
+    while (start > 0 && !/[([<]/.test(text[start - 1])) start--;
+    let end = textArea.selectionEnd;
+    while (end < text.length && !/[)\]>]/.test(text[end])) end++;
+    if (start > 0 && end < text.length -1) {
+        const m = text.substring(start, end).match(/(.*?)(:\s*([\n.]))?/);
+        const rate = ((m[2]) ? parseFloat(m[3]) : 1) + rateGain;
+        if (rate < 0) return;
+        const newText = `${text.slice(0, start)}${m[1]}:${rate.toFixed(2)}${text.slice(end)}`;
+        textArea.value = newText;
+        textArea.setSelectionRange(start, end);
+        textArea.focus();
+    }
+}
+
 
 
 // CSSのインジェクション/解除を切り替える
--- scripts/smartphone-plus.py
+++ scripts/smartphone-plus.py
@@ -7,23 +7,36 @@ def on_ui_tabs():
         gr.Markdown("Customize WebUI for Smartphone. Available only when the client width < 768px.")
         gr.HTML("""
 <div id="sd-smartphone-plus-panel" class="">
-    <button id="sspp-inject-css" class="">
-        <span class="sspp-button-label-off">InjectCSS</span>
-        <span class="sspp-button-label-on">ExtractCSS</span>
-    </button>
-    <button id="sspp-nega-prompt" class="operator">
-        <span class="sspp-button-label">Negative</span>
-    </button>
-    <button id="sspp-config" class="operator">
-        <span class="sspp-button-label">Props</span>
-    </button>
-    <button id="sspp-size" class="operator">
-        <span class="sspp-button-label">Size</span>
-    </button>
-    <div id="sspp-size-selector" class="selector">
-        <button class="selector-item" onclick="sspp_setSize(1440, 1080)">1440x1080</button>
-        <button class="selector-item" onclick="sspp_setSize(1280, 1280)">1280x1280</button>
-        <button class="selector-item" onclick="sspp_setSize(1080, 1440)">1080x1440</button>
+    <div class="sspp-panel-column">
+        <button id="sspp-prevword" class="helper">prev</button>
+        <button id="sspp-currword" class="helper">word</button>
+        <button id="sspp-nextword" class="helper">next</button>
+        <button id="sspp-ratedown" class="helper">-0.05</button>
+        <button id="sspp-rateup"   class="helper">+0.05</button>
+    </div>
+    <div class="sspp-panel-column">
+        <button id="sspp-inject-css" class="">
+            <span class="sspp-button-label-off">InjectCSS</span>
+            <span class="sspp-button-label-on">ExtractCSS</span>
+        </button>
+        <button id="sspp-nega-prompt" class="operator">
+            <span class="sspp-button-label">Negative</span>
+        </button>
+        <button id="sspp-config" class="operator">
+            <span class="sspp-button-label">Props</span>
+        </button>
+        <button id="sspp-size" class="operator">
+            <span class="sspp-button-label">Size</span>
+        </button>
+        <div id="sspp-size-selector" class="selector">
+            <button class="selector-item" onclick="sspp_setSize(1440, 810)">1440x810 (16:9)</button>
+            <button class="selector-item" onclick="sspp_setSize(1440, 960)">1440x960 (3:2)</button>
+            <button class="selector-item" onclick="sspp_setSize(1440, 1080)">1440x1080 (4:3)</button>
+            <button class="selector-item" onclick="sspp_setSize(1280, 1280)">1280x1280 (1:1)</button>
+            <button class="selector-item" onclick="sspp_setSize(1080, 1440)">1080x1440 (3:4)</button>
+            <button class="selector-item" onclick="sspp_setSize(960, 1440)">960x1440 (2:3)</button>
+            <button class="selector-item" onclick="sspp_setSize(810, 1440)">810x1440 (9:16)</button>
+        </div>
     </div>
 </div>
         """)
